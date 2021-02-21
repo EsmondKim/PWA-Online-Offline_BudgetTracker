cc094d7f190243021545ac3511028e7c1af31fc5
EsmondKim <ekelectronicmail@gmail.com>
2021-02-21 16:06:19 -0600

    Updated model name to transactions.

--- models/transaction.js
+++ models/transaction.js
@@ -2,24 +2,22 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema(
-  {
-    name: {
-      type: String,
-      trim: true,
-      required: "Enter a name for transaction"
-    },
-    value: {
-      type: Number,
-      required: "Enter an amount"
-    },
-    date: {
-      type: Date,
-      default: Date.now
-    }
-  }
-);
+const transactionsSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction",
+  },
+  value: {
+    type: Number,
+    required: "Enter an amount",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Transaction = mongoose.model("Transaction", transactionSchema);
+const Transactions = mongoose.model("Transactions", transactionsSchema);
 
-module.exports = Transaction;
+module.exports = Transactions;
